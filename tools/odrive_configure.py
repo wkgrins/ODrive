a30e989a6ff5cc8a9e997ea3e67c56136777e492
wkgrins <willa@grinsfelder.com>
2021-03-22 13:21:54 -0400

    odrive_configure.py includes all of the motor settings and encoder settings for the two-motor dynamometer system, from finding the odrive through calibration settings. dyn_test1.py contains an error exception handling function to detect odrive prior to executing test script.

---
+++ tools/odrive_configure.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Configure odrive connected to two motors for operation in Dynamometer
+All configuration settings are saved to the odrive firmware upon completion.
+"""
+
+from __future__ import print_function
+
+import odrive
+from odrive.enums import *
+import time
+import math
+
+# Find a connected ODrive (this will block until you connect)
+print("finding an odrive...")
+odrv = odrive.find_any()
+
+# Configure Motor Attributes
+odrv.axis0.motor.config.current_lim=40		#current limit, amps
+odrv.axis1.motor.config.current_lim=40
+
+odrv.axis0.controller.config.vel_limit=17		#velocity limit, turns/second
+odrv.axis1.controller.config.vel_limit=17
+
+odrv.axis0.motor.config.pole_pairs=7		#pole pairs in 270KV motors
+odrv.axis1.motor.config.pole_pairs=7
+
+odrv.axis0.motor.config.torque_constant=8.27/270	#torque constant for motors	
+odrv.axis1.motor.config.torque_constant=8.27/270
+	
+odrv.axis0.motor.config.motor_type=MOTOR_TYPE_HIGH_CURRENT
+odrv.axis1.motor.config.motor_type=MOTOR_TYPE_HIGH_CURRENT
+
+#no brake resistor
+#no thermistor
+
+odrv.axis0.encoder.config.cpr=2048*4		#CUI encoders set to max resolution
+odrv.axis1.encoder.config.cpr=2048*4
+
+#Startup Procedure
+odrv.axis0.config.startup_motor_calibration = True
+odrv.axis1.config.startup_motor_calibration = True
+odrv.axis0.config.startup_encoder_offset_calibration=True
+odrv.axis1.config.startup_encoder_offset_calibration=True
+odrv.axis0.config.startup_closed_loop_control=True
+odrv.axis0.config.startup_closed_loop_control=True
+
+# Save updated Odrive configuration
+odrv.save_configuration()
+
+# Confirm end of program 
+print("all done!")
+
+
+#when working configuration is finally loaded, backup using .json file
+#odrivetool backup-config my_config.json
+
+
+
+
+
+
+
+
+
+
